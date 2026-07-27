ba5db746f69c31ea6d960898744d27469071c513
David Castillo <david.castillo4@gmail.com>
2026-07-27 17:59:20 -0500

    tttt

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,20 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Footer({ company, footer }) {
   return (
     <footer className="site-footer">
       <div className="footer-inner">
         <div className="footer-brand-mark">
-          <Image
-            src="/icons/logoNav.png"
-            alt="Motal Well Drilling Services"
-            width={890}
-            height={230}
-            className="footer-logo-image"
-          />
+          <Link href="/admin" className="footer-logo-link" aria-label="Open admin login">
+            <Image
+              src="/icons/logoNav.png"
+              alt="Motal Well Drilling Services"
+              width={890}
+              height={230}
+              className="footer-logo-image"
+            />
+          </Link>
         </div>
 
         <div className="footer-contact-block">
